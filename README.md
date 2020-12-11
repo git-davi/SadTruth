e766639f5c225e96562849b2f99489e6fe454776
git-davi <davidecasalini2@gmail.com>
2020-12-11 12:18:22 +0100

    added multithreading and amplification check

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SadTruth : A simple Open DNS Recursive Resolver list generator
 This simple script will output a list of Open DNS Resolver having recursion enabled.  
-It may take time...
+These servers are likely to be used from hacker or simply disturbed people to do DDOS DNS Amplification attacks.  
+- Added multithreading
 
 ## Usage  
 The simplest use case scenario is to run the script and wait for the list file to be populated with open recursive resolvers.
@@ -8,11 +9,23 @@ The simplest use case scenario is to run the script and wait for the list file t
 $ pip install -r requirements.txt
 $ python SadTruth.py
 ```
+This script is also able to make a smarter analysis and output only servers that are more likely to suffer DNS amplification.  
+To make the list you only have to run :
+```shell
+$ python SadTruth.py --amp
+```
+You can use a custom number of threads.
 
 ## Custom initial lists
-By default the initial dns list is gathered with OSINT and downloaded from `https://public-dns.info/`.  
-But you can use your own by simply replacing the get response content with a `f.read()` or the `url` with your own.  
+By default the initial dns list is gathered with OSINT and downloaded from [here](https://public-dns.info/).  
+But you can supply your own by simply passing the filename to the command line :
+```bash
+$ python SadTruth.py --file my_dns_list.txt
+```
 
 ## Beware Script Kiddies!!
 These dns servers can be used for DNS amplification attacks.  
-DOS is illegal so be *wise and responsible* and **use this tool only for threat intel purpose**. 
+DDOS attacks are illegal so be *wise and responsible* and **use this tool only for threat intel purpose**. 
+
+## Troubleshooting
+The script may not work if you are running it over TOR network.
--- SadTruth.py
+++ SadTruth.py
@@ -6,27 +6,92 @@ import dns.flags
 import dns.name
 import sys
 from alive_progress import alive_bar
+import argparse
+import concurrent.futures
+import threading
 
 
 dns_url = 'https://public-dns.info/nameservers-all.txt'
 domain = dns.name.from_text('google.com')
 
+parser = argparse.ArgumentParser(description="""
+     __           _ _              _   _     
+    / _\ __ _  __| | |_ _ __ _   _| |_| |__  
+    \ \ / _` |/ _` | __| '__| | | | __| '_ \ 
+    _\ \ (_| | (_| | |_| |  | |_| | |_| | | |
+    \__/\__,_|\__,_|\__|_|   \__,_|\__|_| |_|
+                                         
+A simple Open DNS Recursive Resolver list generator.
+This script is able to generate a list of DNS server 
+vulnerable to recursion and which are likely to be 
+used for DDOS DNS amplification attacks.
+""", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--file', type=str, help='Your relative filename, for a custom DNS server list')
+parser.add_argument('--amp', action='store_true', help='If you want to output only servers more likely to suffer DNS Amplification')
+parser.add_argument('-t', dest='threads', type=int, default=5, help='Number of threads')
 
-def is_open_recursive(ip):
+
+found = 0
+progress = 0
+file_lock = threading.Lock()
+bar_lock = threading.Lock()
+
+
+def is_long(resp, threshold=2):
+    length = 0
+    for rec in resp.answer:
+        length += len(rec)
+    return length >= threshold
+
+
+def is_open_recursive(ip, amp):
     # Idenitfy recursive DNS over UDP
+    is_recursive = None
 
-    query = dns.message.make_query(domain, dns.rdatatype.TXT)
-    query.flags |= dns.flags.RD
-    response = dns.query.udp(query, ip, timeout=2)
-    return (response.flags & dns.flags.RA) == dns.flags.RA and len(response.answer) != 0
+    if amp :
+        query = dns.message.make_query(domain, dns.rdatatype.ANY)
+        query.flags |= dns.flags.RD
+        response = dns.query.udp(query, ip, timeout=2)
+        is_recursive = (response.flags & dns.flags.RA) == dns.flags.RA and is_long(response)
+    else :
+        query = dns.message.make_query(domain, dns.rdatatype.TXT)
+        query.flags |= dns.flags.RD
+        response = dns.query.udp(query, ip, timeout=2)
+        is_recursive = (response.flags & dns.flags.RA) == dns.flags.RA and len(response.answer) > 0 
+    return is_recursive
 
 
 def append_file(ip):
+    global found
+
+    file_lock.acquire()
     with open('dns_list.txt', 'a') as f:
         f.write(ip+'\n')
+        found += 1
+    file_lock.release()
+
+
+def update_bar(bar):
+    global progress
+
+    bar_lock.acquire()
+    progress += 1
+    bar()
+    bar_lock.release()
+
+
+def dns_querier(args):
+    ip, amp, bar = args
+    try :
+        if is_open_recursive(ip, amp):
+            append_file(ip)
+    except Exception:
+        pass
+    update_bar(bar)
 
 
 def main():
+    args = parser.parse_args()
     print("[*] Downloading nameserver list")
     r = requests.get(dns_url)
     dns_ips = r.text.split()
@@ -36,22 +101,20 @@ def main():
     print("[*] Found {} dns ips".format(len(dns_ips)))
     print("[*] Begin recursive server discovery phase")
     
-    recursive = 0
-    progress = 0
     with alive_bar(len(dns_ips)) as bar:
-        for ip in dns_ips:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
             try:
-                if is_open_recursive(ip):
-                    append_file(ip)
-                    recursive += 1
+                args_array = [ (ip, args.amp, bar) for ip in dns_ips ]
+                futures = executor.map(dns_querier, args_array)
+                concurrent.futures.wait(futures)
             except KeyboardInterrupt:
                 print('[x] Interrupting')
-                print('[*] Found {} open recursive DNS out of {} tested'.format(recursive, progress))
+                print('[*] Found {} open recursive DNS out of {} tested'.format(found, progress))
+                executor.shutdown(wait=False)
                 sys.exit(0)
-            except Exception :
-                pass
-            progress += 1
-            bar()
+            except Exception as e :
+                print(e)
+            
 
 
 if __name__ == "__main__":
